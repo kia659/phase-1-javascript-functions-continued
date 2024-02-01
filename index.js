ac4246a382e8b7b5593e46d6899283668b66a01b
Kia Neitzell <kia.neitzell@gmail.com>
2024-01-31 17:20:42 -0800

    compleated

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate"){
+return `This Saturday, I want to ${fun}!`;
+
+}// code your solution here
+saturdayFun()
+
+
+function mondayWork(work = "go to the office"){
+return `This Monday, I will ${work}.`
+
+}
+mondayWork()
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
